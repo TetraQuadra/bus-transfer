bf81535f9c6de384e04bfb736be46f200e64d926
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-08-15 13:34:51 +0300

    i18n: popularRoutesSection

--- src/sections/PopularRoutesSection.tsx
+++ src/sections/PopularRoutesSection.tsx
@@ -1,40 +1,29 @@
-'use client'
-
 import Image from 'next/image';
+import { getTranslations } from 'next-intl/server';
 
 // TODO: ВОЗМОЖНО КЛИКАБЕЛЬНЫЕ ССЫЛКИ НА МАРШРУТЫ
 
-export const POPULAR_ROUTES = {
-    title: "ПОПУЛЯРНІ НАПРЯМКИ",
-    countries: [
-        "Польща",
-        "Німеччина",
-        "Нідерланди",
-        "Бельгія",
-    ],
-    cities: ["Умань", "Біла Церква", "Київ", "Житомир", "Вінниця", "Хмельницький", "Тернопіль", "Львів", "Рівне", "Луцьк", "Звигель"
-    ]
-};
-
-
-const PopularRoutesSection = () => {
+const PopularRoutesSection = async () => {
+    const t = await getTranslations('popularRoutes');
+    const countries = t.raw('countries') as string[];
+    const cities = t.raw('cities') as string[];
     return (
         <section className="py-16 w-full">
             <div className="">
                 <div className="w-full">
                     <h2 className="text-[40px] font-regular text-center text-foreground mb-12">
-                        {POPULAR_ROUTES.title}
+                        {t('title')}
                     </h2>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
-                        {POPULAR_ROUTES.countries.map((country) => (
+                        {countries.map((country) => (
                             <div key={country} className="flex flex-col">
                                 <h3 className="text-[24px] font-medium text-foreground mb-4">
                                     {country}
                                 </h3>
 
                                 <div className="space-y-[20px]">
-                                    {POPULAR_ROUTES.cities.map((city, index) => (
+                                    {cities.map((city, index) => (
                                         <div key={index} className="flex items-center gap-2">
                                             <Image
                                                 src="/icons/triangleRight.svg"
