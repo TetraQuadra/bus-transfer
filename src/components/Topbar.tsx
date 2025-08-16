1414f7bf41a47380bc065276f7dfd3864b890f0a
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-08-16 19:07:47 +0300

    Update Topbar.tsx

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 const Topbar = () => {
     //TODO: add links
     return (
-        <div className="flex gap-1 items-center justify-center py-2 bg-[var(--color-secondary)] w-full">
-            <span className="text-white">
+        <div className="flex items-center justify-center bg-[var(--color-secondary)] w-full py-1 sm:py-2 gap-1 sm:gap-3">
+            <span className="text-white text-xs sm:text-sm md:text-base">
                 Микола
             </span>
-            <Image className="mx-2" quality={100} alt="kyivstar logo" src="/icons/kyivstar.png" width={32} height={32} />
-            <Image className="mx-2 " quality={100} alt="viber logo" src="/icons/viber.png" width={32} height={32} />
-            <a href="tel:+380982275197" className="text-white mr-[36px]">+38 (098) 227 51 97</a>
-            <Image className="mx-2" quality={100} alt="instagram logo" src="/icons/inst.png" width={32} height={32} />
-            <Image className="mx-2" quality={100} alt="telegram logo" src="/icons/tg.png" width={32} height={32} />
-            <Image className="mx-2" quality={100} alt="whatsapp logo" src="/icons/whatsapp.png" width={32} height={32} />
+            <Image className="mx-1 sm:mx-2 max-sm:w-4 max-sm:h-4" quality={100} alt="kyivstar logo" src="/icons/kyivstar.png" width={32} height={32} />
+            <Image className="mx-1 sm:mx-2 max-sm:w-4 max-sm:h-4" quality={100} alt="viber logo" src="/icons/viber.png" width={32} height={32} />
+            <a href="tel:+380982275197" className="text-white text-xs sm:text-sm md:text-base mr-2 sm:mr-9 whitespace-nowrap">+38 (098) 227 51 97</a>
+            <Image className="mx-1 sm:mx-2 max-sm:w-4 max-sm:h-4" quality={100} alt="instagram logo" src="/icons/inst.png" width={32} height={32} />
+            <Image className="mx-1 sm:mx-2 max-sm:w-4 max-sm:h-4" quality={100} alt="telegram logo" src="/icons/tg.png" width={32} height={32} />
+            <Image className="mx-1 sm:mx-2 max-sm:w-4 max-sm:h-4" quality={100} alt="whatsapp logo" src="/icons/whatsapp.png" width={32} height={32} />
         </div>
     );
 };
