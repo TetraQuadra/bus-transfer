e8475dda6028ed9b5241d22b9b759528d6c7f0db
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-08-16 18:17:48 +0300

    style: popularRoutes adaptation

--- src/sections/PopularRoutesSection.tsx
+++ src/sections/PopularRoutesSection.tsx
@@ -8,14 +8,14 @@ const PopularRoutesSection = async () => {
     const countries = t.raw('countries') as string[];
     const cities = t.raw('cities') as string[];
     return (
-        <section className="py-16 w-full">
+        <section className="py-16 w-full max-sm:px-1">
             <div className="">
                 <div className="w-full">
                     <h2 className="text-[40px] font-regular text-center text-foreground mb-12">
                         {t('title')}
                     </h2>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
+                    <div className="grid max-sm:grid-cols-2 max-lg:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
                         {countries.map((country) => (
                             <div key={country} className="flex flex-col">
                                 <h3 className="text-[24px] font-medium text-foreground mb-4">
@@ -24,15 +24,15 @@ const PopularRoutesSection = async () => {
 
                                 <div className="space-y-[20px]">
                                     {cities.map((city, index) => (
-                                        <div key={index} className="flex items-center gap-2">
+                                        <div key={index} className="flex items-center gap-2 max-sm:gap-1">
                                             <Image
                                                 src="/icons/triangleRight.svg"
                                                 alt=""
                                                 width={20}
                                                 height={20}
-                                                className="object-contain"
+                                                className="object-contain max-sm:w-[16px] max-sm:h-[16px]"
                                             />
-                                            <span className="text-[16px] font-regular text-foreground">
+                                            <span className="text-[16px] font-regular text-foreground max-sm:text-[12px]">
                                                 {city} - {country}
                                             </span>
                                         </div>
