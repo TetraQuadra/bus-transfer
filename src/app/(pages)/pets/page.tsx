84ad6afe654790b5f61555ff4a43c278f6078076
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-09-25 18:05:19 +0300

    feat: seo for /pets route

--- src/app/(pages)/pets/page.tsx
+++ src/app/(pages)/pets/page.tsx
@@ -6,7 +6,37 @@ import PetsServiceAdvantages from "@/sections/PetsServiceAdvantages";
 import PetsTitle from "../../../sections/PetsTitle";
 import { getTranslations } from "next-intl/server";
 import BookingSection from "@/sections/BookingSection";
+import type { Metadata } from "next";
 
+export async function generateMetadata(): Promise<Metadata> {
+    const t = await getTranslations('pets.meta');
+
+    const title = t('title');
+    const description = t('description');
+    const keywords = t('keywords') as unknown as string[];
+
+    return {
+        title,
+        description,
+        keywords,
+        robots: {
+            index: true,
+            follow: true
+        },
+        openGraph: {
+            title,
+            description,
+            type: 'website',
+            images: ['/logo.png']
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            images: ['/logo.png']
+        }
+    };
+}
 
 export default async function PetsPage() {
     const t = await getTranslations('pets');
