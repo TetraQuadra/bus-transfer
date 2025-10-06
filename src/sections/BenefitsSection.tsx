989fd6c258ab6ba19afd544b080f5afc0a086a98
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-10-06 14:21:53 +0300

    Update BenefitsSection.tsx

--- src/sections/BenefitsSection.tsx
+++ src/sections/BenefitsSection.tsx
@@ -13,7 +13,7 @@ const BenefitsSection = () => {
         <section id="benefits" className="w-full bg-[var(--color-secondary)] rounded-[10px] shadow-lg mb-15 md:mb-16 py-8 sm:py-10 md:py-12 lg:py-14">
             <div className="w-full">
                 <div className="max-w-7xl mx-auto px-4">
-                    <h2 className="text-[30px] sm:text-[28px] md:text-[32px] lg:text-[36px] font-regular text-white text-left mb-6 sm:mb-8">
+                    <h2 className="text-[30px] sm:text-[28px] md:text-[32px] lg:text-[36px] font-regular text-white text-left mb-6 sm:mb-8 md:text-center">
                         {t('title')}
                     </h2>
 
