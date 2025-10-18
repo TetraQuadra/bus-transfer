c00848137f3540c26d3dfd40d8356e93c00cde6f
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-10-18 13:21:58 +0300

    change: new icons

--- src/sections/Autopark.tsx
+++ src/sections/Autopark.tsx
@@ -13,6 +13,13 @@ const Autopark = () => {
         description: t(`features.${activeTab}.${i}.description`)
     }));
 
+    const icons = [
+        '/icons/shield.svg',
+        '/icons/play.svg',
+        '/icons/5star.svg',
+        '/icons/wifi.svg'
+    ];
+
     return (
         <section id="fleet" className="w-full mb-15 md:mb-[140px]">
             <h2 className="text-[30px] lg:text-[40px] font-regular text-center text-foreground mb-8 sm:mb-10 lg:mb-12 uppercase">{t('title')}</h2>
@@ -44,7 +51,7 @@ const Autopark = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[60px] gap-y-[30px]">
                     {currentTexts.map((text, index) => (
                         <div key={index} className="flex flex-row gap-[14px] items-start">
-                            <Image src="/icons/shield.svg" alt={t('alt.icon')} width={70} height={70} quality={100} className="w-10 h-10 md:w-[70px] md:h-[70px]" />
+                            <Image src={icons[index]} alt={t('alt.icon')} width={70} height={70} quality={100} className="w-10 h-10 md:w-[70px] md:h-[70px]" />
                             <div>
                                 <h3 className="text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-medium text-foreground mb-1 pt-[16px] max-md:pt-0 uppercase">{text.title}</h3>
                                 <p className="text-[14px] sm:text-[15px] md:text-[16px] font-regular text-foreground" dangerouslySetInnerHTML={{ __html: text.description.replace(/<br\/>/g, '<br/>') }}></p>
